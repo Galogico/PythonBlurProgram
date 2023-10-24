e6fbb7c58038c1851c85219e01406c500f1585df
cafalchio <mcafalchio@gmail.com>
2023-10-24 23:06:43 +0100

    fix some blur are happening

--- GaussianBlur.py
+++ GaussianBlur.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import math
 import numpy as np
+from tqdm import tqdm
 
 
 def gaussian_2d(x, y, std_dev):
@@ -9,8 +10,9 @@ def gaussian_2d(x, y, std_dev):
     # Formula desse site
     # https://www.geeksforgeeks.org/apply-a-gauss-filter-to-an-image-with-python/
     a = 1 / (2 * math.pi * std_dev**2)
-    e =  math.exp(-(x**2 + y**2) / (2*std_dev**2))
+    e = math.exp(-(x**2 + y**2) / (2 * std_dev**2))
     return a * e
+
 
 def create_gaussian_kernel(size=5, std_dev=3):
     """Create kernel 2d"""
@@ -22,7 +24,7 @@ def create_gaussian_kernel(size=5, std_dev=3):
         for j in range(size):
             x = i - center
             y = j - center
-            row.append(gaussian_2d(x,y, std_dev))
+            row.append(gaussian_2d(x, y, std_dev))
         kernel.append(row)
     # increase to > 1
     factor = min(list(min(kernel)))
@@ -34,58 +36,52 @@ def create_gaussian_kernel(size=5, std_dev=3):
     for i in range(size):
         for j in range(size):
             total += kernel[i][j]
-    print(total)
     return kernel, total
+
 
 def create_box_kernel(size=5):
     """Create a box kernel"""
-    kernel = []
+    k = []
     for _ in range(size):
-        kernel.append([1] * size)
-    return kernel, size**2
+        k.append([1] * size)
+    return k, size**2
 
 
-def save_array_image(image_arr):
-    """ save image from numpy array"""
-    img = Image.fromarray(image_arr.astype('uint8'), 'RGB')
-    img.save("output.jpg")
-    
-
-
-def apply_kernel_around(img, rgb, x, y, kernel, size, total):
-    """ tentativa de aplicar o kernel"""
+def apply_kernel_around_pixel(x, y, img, rgb, kernel, size, total):
+    """tentativa de aplicar o kernel"""
     new_pixel = 0
     for i in range(0, size):
         for j in range(0, size):
-            new_pixel += kernel[i][j] * img[i-size//2][j-size//2][rgb] 
-            print(f"image coord: rgb {rgb} x:{x-i-size//2}, y: {y-j-size//2} = {new_pixel/total}")
+            new_pixel += kernel[i][j] * img[x + i - size // 2][y + j - size // 2][rgb]
     return new_pixel / total
 
 
-
-def run_gaussian_filter(size=3, std=.5):
-    """ run gaussian filter in image no padding yet"""
-
-    kernel, total = create_gaussian_kernel(size, std)
-    
-    try:
-        im = Image.open(r"./image/eye.jpg")
-    except Exception as e:
-        print(f"{'*' * 40}\n{e}\n{type(e)}\nProblem reading image:")
-    
+def apply_filter(im, kernel, total):
+    """Apply filter"""
     w, h = im.size
-
     img = np.array(im)
-    for rgb in range(3):
-        for i in range(size // 2 + 1, h - size // 2 ):
-            for j in range(size // 2 + 1, w - size //2): # Nao faz as bordas
-                img[i][j][rgb] = apply_kernel_around(img, rgb, i, j, kernel, size, total)
+    size = len(kernel[0])
+    for rgb in tqdm(range(3)):
+        for i in range(size // 2, h - size // 2):
+            for j in range(size // 2, w - size // 2):
+                img[i][j][rgb] = apply_kernel_around_pixel(
+                    i, j, img, rgb, kernel, size, total
+                )
+    return img
 
-    save_array_image(img)
 
-    
+def load_image(filename):
+    """Load image"""
+    im = Image.open(filename)
+    return im
 
-    
+
+def save_array_image(image_arr, out_name="output.jpg"):
+    """save image from numpy array"""
+    img = Image.fromarray(image_arr)
+    img.save(out_name)
+    print("image saved.")
+
 
 # rList = []
 # gList = []
@@ -167,4 +163,17 @@ def run_gaussian_filter(size=3, std=.5):
 
 
 if __name__ == "__main__":
-    run_gaussian_filter(size=3, std=.5)
+    kernel, total = create_box_kernel(size=3)
+    img = load_image("./image/eye.jpg")
+
+    save_array_image(img)
+
+    # passar varias vezes
+    # vezes = 10
+    # kernel, total = create_box_kernel(size=3)
+    # img = load_image("./image/eye.jpg")
+    # img = apply_filter(img, kernel, total)
+    # for i in range(vezes):
+    #     if i == vezes - 1:
+    #         break
+    # img = Image.fromarray(img)
